09762bbbbe36d32cdad5ae9950940f5fb4ec1ffa
jayasurya1501 <veejaisurya@gmail.com>
2023-12-18 11:49:42 +0530

    c1

---
+++ script.js
@@ -0,0 +1,56 @@
+
+setTimeout(function () {
+    document.body.innerHTML = 10;
+  
+    setTimeout(function () {
+      document.body.innerHTML = 9;
+  
+      setTimeout(function () {
+        document.body.innerHTML = 8;
+  
+        setTimeout(function () {
+          document.body.innerHTML = 7;
+  
+          setTimeout(function () {
+            document.body.innerHTML = 6;
+  
+            setTimeout(function () {
+              document.body.innerHTML = 5;
+  
+              setTimeout(function () {
+                document.body.innerHTML = 4;
+  
+                setTimeout(function () {
+                  document.body.innerHTML = 3;
+  
+                  setTimeout(function () {
+                    document.body.innerHTML = 2;
+  
+                    setTimeout(function () {
+                      document.body.innerHTML = 1;
+  
+                      setTimeout(function () {
+                        
+                        document.body.innerHTML = "Happy Independence Day";
+                      }, 1000);
+  
+                    }, 1000);
+  
+                  }, 1000);
+  
+                }, 1000);
+  
+              }, 1000);
+  
+            }, 1000);
+  
+          }, 1000);
+  
+        }, 1000);
+  
+      }, 1000);
+  
+    }, 1000);
+  
+  }, 1000);
+  
